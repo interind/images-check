c227097710921849f758796f82d5da053d6d2550
Aleksander <interind@yandex.ru>
2021-07-07 13:42:30 +0300

    correct

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,23 +1,45 @@
 <template>
-  <div class="cards">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
-      <img class="logo" alt="Vue logo" src="../assets/logo.png">
+  <template v-if="srcImages.length > 0">
+    <div class="cards">
+      <img
+        v-for="srcImage in srcImages"
+        class="card"
+        :alt="srcImage.name"
+        :src="srcImage.link"
+        :key="srcImage.name"
+        @mousedown="togglePopup"
+      >
     </div>
+  </template>
+  <template v-else>
+    <div class="cards">
+      <span class="cards__title">Сдесь будут показаны ваши файлы</span>
+    </div>
+  </template>
+  <template v-if="popupStatus">
+    <div class="popup popup_opened" @mousedown="togglePopup">
+      <img class="popup__pic" :src="src" alt="title">
+    </div>
+  </template>
 </template>
 
 <script>
-export default { name: 'Cards' };
-
+export default {
+  name: 'Cards',
+  props: {
+    srcImages: Array,
+  },
+  data: () => ({
+    popupStatus: false,
+    src: '',
+    title: '',
+  }),
+  methods: {
+    togglePopup(evt) {
+      this.popupStatus = !this.popupStatus;
+      this.src = evt.target.src;
+      this.title = evt.target.alt;
+    },
+  },
+};
 </script>
--- src/components/ContentBlock.vue
+++ src/components/ContentBlock.vue
@@ -1,9 +1,44 @@
 <template>
   <div class="content">
     <h1 class="content__title">{{ msg }}</h1>
-    <div class="content__check"><span class="content__mark">Перенесите файл сюда</span></div>
-    <button class="button button_type_check" type="button">Отправить</button>
-    <Cards />
+    <div :class="classActive">
+      <input
+        @drop="onDragDrop"
+        @dragover="onDragOver"
+        @change="isStatusInput"
+        type="file"
+        class="content__input"
+        id="input-image"
+        multiple
+        accept="image/*"
+      >
+      <template v-if="status">
+        <span class="content__mark">{{ messageStatus }}</span>
+      </template>
+      <template v-else>
+        <span class="content__mark">{{ messageStatus }}</span>
+      </template>
+    </div>
+    <template v-if="status">
+      <button
+        class="button button_type_check"
+        @click="onAddImages"
+        type="button"
+      >
+        Отправить
+      </button>
+    </template>
+    <template v-else>
+      <button
+        class="button button_type_check"
+        @click="onAddImages"
+        type="button"
+        disabled
+      >
+        Отправить
+      </button>
+    </template>
+    <Cards :srcImages= "srcImages" />
   </div>
 </template>
 
@@ -14,8 +49,71 @@ import Cards from './Cards.vue';
 export default {
   components: { Cards },
   name: 'ContentBlock',
+  data: () => ({
+    srcImages: [],
+    status: false,
+    classActive: 'content__check',
+    messageStatus: 'Перенесите сюда файл',
+  }),
+  filesNames: [],
+  fileLoader: [],
   props: {
     msg: String,
   },
+  methods: {
+    isStatusInput(evt) {
+      if (evt.target.value) {
+        this.status = true;
+        this.messageStatus = 'Файлы получены';
+        this.classActive = 'content__check content__check_active';
+      } else {
+        this.status = false;
+        this.classActive = 'content__check';
+      }
+    },
+    onAddImages() {
+      if (this.status) {
+        this.readerImages(this.fileLoader);
+        this.status = false;
+        this.messageStatus = 'Перенесите сюда файл';
+        this.classActive = 'content__check';
+      }
+    },
+    readerImages(files) {
+      files.forEach((file) => {
+        const result = [];
+        const reader = new FileReader(file);
+        reader.onload = () => {
+          this.srcImages.push({ link: reader.result, name: file.name });
+          result.push({ code: reader.result, nameFile: file.name, size: file.size });
+        };
+        reader.readAsDataURL(file);
+        console.log(result);
+      });
+    },
+    onDragDrop(evt) {
+      this.filesNames = [...Array(this.filesNames || '')];
+      const arrayFiles = Array.from(evt.dataTransfer.files).filter((file) => file.type.startsWith('image/'));
+      const arrayCorrect = [];
+      arrayFiles.forEach((el) => {
+        if (this.filesNames.length > 0 && this.filesNames.find((e) => e.includes(el.name))) {
+          this.classActive = 'content__check content__check_error';
+          this.messageStatus = 'Файлы уже были добавлены';
+        } else {
+          this.filesNames.push(el.name);
+          arrayCorrect.push(el);
+        }
+      });
+      if (arrayCorrect.length > 0) {
+        this.fileLoader = arrayCorrect;
+      } else {
+        evt.preventDefault();
+      }
+    },
+    onDragOver(evt) {
+      evt.preventDefault();
+    },
+  },
 };
+
 </script>
